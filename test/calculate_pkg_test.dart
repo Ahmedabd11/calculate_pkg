558516aa9422d78b3fd5ae73c0eb412afa5c93af
AhmedAbdullah <mrahmedabdullah876@gmail.com>
2023-01-27 11:06:52 +0500

    first commit

---
+++ test/calculate_pkg_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:calculate_pkg/calculate_pkg.dart';
+
+void main() {
+  test('adds one to input values', () {
+    // final calculator = CalculateWidget();
+  });
+}
