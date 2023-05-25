9e4847d8dd504a58bcf49f3bb99601ce369f6b1e
AhmedAbdullah <ahmed.abdullah@evampsaanga.com>
2023-05-25 11:23:03 +0500

    food package added

--- lib/food_pkg.dart
+++ lib/food_pkg.dart
@@ -2,32 +2,17 @@
 library food_pkg;
 
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '';
-import 'App/users_list/view_model/user_view_model.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 /// A Food App.
 // typedef ResponseCallBack=void Function(String balence);
 // typedef TapCallBack= Future<String> Function(ResponseCallBack balence);
-class Food extends StatelessWidget {
-  final Information information;
-  const Food({Key key, this.information}) :super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_)=> UserViewModel())],
-      child: FoodWidget(information: information,),
-    );
-  }
-}
 
-class FoodWidget extends StatefulWidget {
-  final Function balanceCallBack;
-  final Information information;
-  const FoodWidget({Key key, this.information,this.balanceCallBack}) : super(key: key);
-
-  @override
-  State<FoodWidget> createState() => _FoodWidgetState();
+class MenuFood{
+  String name;
+  String image;
+  MenuFood({this.name, this.image});
 }
 
 class Information{
@@ -35,149 +20,201 @@ class Information{
   String number;
   Information({this.name, this.number});
 }
-class MenuFood{
-  String name;
-  Icon icon;
-  MenuFood({this.name, this.icon});
-}
-
-class _FoodWidgetState extends State<FoodWidget> {
-  String enteredText = '';
-  TextEditingController emailController=TextEditingController();
-  String superText='';
-  String operator = '';
-  double result = 0;
+class Food extends StatelessWidget {
+  final Information information;
+  Food({Key key, this.information}) :super(key: key);
+  TextEditingController emailController = TextEditingController();
+  final List<MenuFood> menuData = [
+    MenuFood(name: 'Burger', image: "assets/test/burg.png"),
+    MenuFood(name: 'Pizza', image: "assets/test/pizza.png"),
+    MenuFood(name: 'Soup', image: "assets/test/soup.png"),
+    MenuFood(name: 'Roll', image: "assets/test/roll.png"),
+  ];
   final List<MenuFood> data = [
-    MenuFood(name: 'Burger',icon:const Icon(Icons.fastfood) ),
-    MenuFood(name: 'Juice',icon:const Icon(Icons.emoji_food_beverage) )
+    MenuFood(name: 'Beef Burger', image: "assets/test/burger.png"),
+    MenuFood(name: 'American Burger', image: "assets/test/burger.png"),
   ];
   int i = 1;
-
   @override
   Widget build(BuildContext context) {
-    UserViewModel userViewModel=context.watch<UserViewModel>();
     return Scaffold(
-      backgroundColor: const Color(0xff22252D),
+      resizeToAvoidBottomInset: true,
+      backgroundColor: const Color(0xffFDF8FF),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 40),
-           const Text('Find Your',style: TextStyle(color: Colors.grey,fontSize: 16),),
-            const SizedBox(height: 10),
-            const Text('Favorite Food',style: TextStyle(color: Colors.black,fontSize: 36),),
-            const SizedBox(height: 20),
-          Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.fromLTRB(30, 3, 20, 0),
-            margin: EdgeInsets.only(left: 10, right: 10),
-            height: 50,
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10)),
-            child: TextField(
-              keyboardType: TextInputType.emailAddress,
-              cursorColor: Colors.black,
-              controller: emailController,
-              textInputAction: TextInputAction.search,
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                suffixIcon: IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.email_outlined),
-                  color: Colors.grey,
-                ),
-                hintText: 'Correo Electrónico',
-                hintStyle: TextStyle(color: Colors.black),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('assets/test/menu.png'),
               ),
-              style: TextStyle(color: Colors.black),
-            )),
-            const SizedBox(height: 20),
-            SizedBox(
-            width: 327,
-            height: 153,
-            child: ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: data.length,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return Container(
-                    width: 50,
-                    height: 50,
-                    padding: const EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(22)),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          width: 24,
-                            height: 23,
-                            child: data[index].icon),
-                        const SizedBox(
-                          height: 13,
-                        ),
-                        Text(
-                          data[index].name,
-                          style: const TextStyle(color: Colors.black,
-                          ),
-                        ),
-                      ],
+              const SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:  [
+                  Text(
+                    'Find Your',
+                    style: GoogleFonts.poppins(textStyle: const TextStyle(color: Colors.grey, fontSize: 16)),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 4),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:  [
+                  Text(
+                    'Favorite Food',
+                    style: GoogleFonts.poppins(textStyle: const TextStyle(color: Colors.black, fontSize: 36,fontWeight: FontWeight.bold),),
+
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.fromLTRB(30, 3, 20, 0),
+                  margin: const EdgeInsets.only(left: 10, right: 10),
+                  height: 50,
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                          color: const Color(0xffDBDBDB)
+                      ),
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    cursorColor: Colors.black,
+                    controller: emailController,
+                    textInputAction: TextInputAction.search,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      suffixIcon: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.search),
+                        color: Colors.grey,
+                      ),
+                      hintText: 'Search here',
+                      hintStyle: GoogleFonts.poppins(textStyle:const TextStyle(color: Color(0xffDBDBDB)) ) ,
                     ),
-                  );
-                })),
-            const SizedBox(height: 50),
-             SizedBox(
-               width: 327,
-               height: 400,
-               child:GridView.builder(
-                   gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                       maxCrossAxisExtent: 200,
-                       childAspectRatio: 0.9,
-                       crossAxisSpacing: 20,
-                       mainAxisSpacing: 20),
-                   itemCount: data.length,
-                   itemBuilder: (BuildContext ctx, index) {
-                     return Container(
-                       width: 155.5,
-                       height: 165,
-                       padding: EdgeInsets.all(16),
-                       decoration: BoxDecoration(
-                           color: Colors.white,
-                           borderRadius: BorderRadius.circular(12)),
-                       child: Column(
-                         crossAxisAlignment: CrossAxisAlignment.start,
-                         children: [
-                           Container(
-                               width: 124,
-                               height: 123,
-                               child: data[index].icon),
-                           SizedBox(
-                             height: 13,
-                           ),
-                           Row(
-                             mainAxisAlignment: MainAxisAlignment.center,
-                             children: [
-                               Text(
-                                 data[index].name,
-                                 style:TextStyle(color: Colors.black)
-                               ),
-                             ],
-                           ),
-                         ],
-                       ),
-                     );
-                   }),
+                    style: const TextStyle(color: Colors.black),
+                  )),
+              const SizedBox(height: 20),
+              Container(
+                  width: double.infinity,
+                  height: 153,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                          color: const Color(0xffDBDBDB)
+                      ),
+                      borderRadius: BorderRadius.circular(22)),
+                  child: ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: menuData.length,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          height: 50,
+                          width: MediaQuery.of(context).size.width/4,
+                          padding: const EdgeInsets.all(16),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Center(child: Image.asset(menuData[index].image??"")),
+                              const SizedBox(
+                                height: 13,
+                              ),
+                              Center(
+                                child: Text(
+                                  menuData[index].name??'',
+                                  style:GoogleFonts.poppins(textStyle: const TextStyle(
+                                    color: Colors.black,
+                                  )),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Center(
+                                child: Text(
+                                  '|',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: index==0?const Color(0xff00B8D9):Colors.black,
+                                  ),),
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Center(
+                                child: Text(
+                                    '15',
+                                    style:GoogleFonts.poppins(textStyle: const TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.black,
+                                    ),)),
+                              ),
+                            ],
+                          ),
+                        );
+                      })),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.only(left: 12),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 290,
+                  child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: data.length,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          width: 200,
+                          height: 290,
+                          padding: const EdgeInsets.all(16),
+                          margin:const EdgeInsets.only(right: 20) ,
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: const Color(0xffDBDBDB)
+                              ),
+                              color: index==0?const Color(0xff690090):Colors.white,
+                              borderRadius: BorderRadius.circular(12)),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Center(child: Image.asset(data[index].image??"")),
+                              const SizedBox(
+                                height: 23,
+                              ),
+                              Text(data[index].name??'',
+                                  style:GoogleFonts.poppins(textStyle: TextStyle(color:  index==0?Colors.white:Colors.black,fontWeight: FontWeight.bold,fontSize: 16))),
+                              const SizedBox(
+                                height: 13,
+                              ),
+                              Row(
+                                children:  [
+                                  const Icon(Icons.star,color: Color(0xff00B8D9),size: 15,),
+                                  const SizedBox(width: 10,),
+                                  Text('4.9 - 20 mins',
+                                      style:GoogleFonts.poppins(textStyle: TextStyle(color: index==0?Colors.white:Colors.black,fontWeight: FontWeight.bold,fontSize: 10))),
+                                ],
+                              )
 
-
-             )
-
-          ],
-        )
-      ),
+                            ],
+                          ),
+                        );
+                      }),
+                ),
+              )
+            ],
+          )),
     );
   }
 }
+
+
+
+
